d568f877fc25283b8ab92733753a6f17a0254739
Gergely Nagy <algernon@madhouse-project.org>
2011-11-08 23:59:34 +0100

    Initial import

---
+++ extension.js
@@ -0,0 +1,7 @@
+const Main = imports.ui.main;
+const Gettext = imports.gettext.domain('gnome-shell');
+const _ = Gettext.gettext;
+
+function main(metadata) {
+    Main.panel._statusmenu._user.get_real_name = function () { return _("System"); };
+}
